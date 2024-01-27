d8ba5df77484e1b9fbf6122a5ec59c8a470de50a
arjun vr <arjarjun2004@gmail.com>
2024-01-27 12:12:05 +0530

    ch

--- frontend/src/components/userComponents/Loader.tsx
+++ frontend/src/components/userComponents/Loader.tsx
@@ -3,7 +3,7 @@
 const Loader = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-50">
-      <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-24 w-24"></div>
+      <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-100 h-24 w-24"></div>
     </div>
   );
 };
