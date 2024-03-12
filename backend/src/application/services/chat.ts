139b29f449ec594f0adf9e9adc1969e28b0690c0
arjun vr <arjarjun2004@gmail.com>
2024-03-13 01:32:24 +0530

    rmeoved the rtc functions

--- backend/src/application/services/chat.ts
+++ backend/src/application/services/chat.ts
@@ -8,29 +8,6 @@ export const chatConnect = async()=>{
         socket.on('chat',()=>{
           console.log('chat')
         })
-
-        socket.on('joinRequest',(channel:string)=>{
-          console.log(`a creator started live on ${channel} channel check`)
-          io.emit('invite',channel)
-        })
-
-        socket.on('joined',()=>{
-          io.emit('newjoin')
-        })
-        socket.on('offer',(offer:RTCSessionDescriptionInit)=>{
-          console.log('offer sent success')
-          io.emit('offersent',offer)
-
-        })
-
-        socket.on('answer',(answer:RTCSessionDescriptionInit)=>{
-          console.log('getting')
-          io.emit('answersent',answer)
-        })
-        socket.on('ice',(candidate:RTCIceCandidate)=>{
-          console.log('ice is triggering')
-          io.emit('icesent',candidate)
-        })
         socket.on('send',async(data)=>{
             const {message,userName,image,channel} = data
 
--- frontend/src/components/userComponents/LiveStreamComponent.tsx
+++ frontend/src/components/userComponents/LiveStreamComponent.tsx
@@ -3,8 +3,7 @@ import { connect } from "react-redux";
 import { io } from "socket.io-client";
 
 export const LiveStreamComponent = () => {
-  const [remoteStream,setRemoteStream] = useState<MediaStream|null>(null)
-  const peerConnectionRef = useRef<RTCPeerConnection>()
+
 
   const socket = io("http://localhost:8000");
 
@@ -15,112 +14,6 @@ export const LiveStreamComponent = () => {
         }
     ]
 }
-
-
-useEffect(()=>{
- 
-  socket.on('offersent',(offer)=>{
-
-    handleOffer(offer)
-
-  })
-  return ()=>{
-    socket.off('offer',handleOffer)
-  }
-},[])
-
-const handleOffer =async(offer:RTCSessionDescriptionInit)=>{
-  try {
-    console.log('offer:',offer)
-    peerConnectionRef.current = new RTCPeerConnection(servers)
-    console.log('state:',peerConnectionRef.current?.signalingState)
-    console.log('connectin state:',peerConnectionRef.current.connectionState)
-    await peerConnectionRef.current.setRemoteDescription(offer)
-    console.log('state:',peerConnectionRef.current?.signalingState)
-    console.log('connectin state:',peerConnectionRef.current.connectionState)
-
-
-    await createAnswer()
-
-  } catch (error) {
-    console.log(error)
-  }
-
-}
-
-
-
-const createAnswer =async()=>{
-  try{
-
-    const answer = await peerConnectionRef.current?.createAnswer()
-  console.log('state:',peerConnectionRef.current?.signalingState)
-
-  console.log(peerConnectionRef.current,'poedfjlsa')
-
-    await peerConnectionRef.current?.setLocalDescription(answer)
-  console.log('state:',peerConnectionRef.current?.signalingState)
-  console.log('connectin state:',peerConnectionRef.current?.connectionState)
-
-  if(peerConnectionRef.current){
-    console.log('enehf')
-    peerConnectionRef.current.ontrack = (event)=>{
-      console.log('new track added;',event.track)
-    }
-  }
-
-  console.log(peerConnectionRef.current,'just checking')
-
-
-  
-    socket.emit('answer',answer)
-
-
-    if(peerConnectionRef.current){
-      console.log('getting in.....')
-      peerConnectionRef.current.ontrack = (event) => {
-        console.log('ontrack event triggered');
-        if (event.streams && event.streams.length > 0) {
-          console.log('Remote stream received:', event.streams[0]);
-          setRemoteStream(event.streams[0]);
-        }
-      };
-    }
-
-    console.log(remoteStream,'remotestream')
-  }catch(error){
-    console.log(error)
-  }
- 
-  
-}
-
-
-
-
-
-
-
-
-useEffect(()=>{
-  socket.on('icesent',(candidate:RTCIceCandidate)=>{
-    console.log('ice candidate:',candidate)
-
-    const connect =()=>{
-      try{
-        if(peerConnectionRef.current){
-          peerConnectionRef.current.addIceCandidate(candidate)
-          console.log('candidate:',candidate)
-        }
-      }catch(error){
-        console.log(error)
-      }
-
-    }
-    connect()
-  })
-  socket.off('icesent',connect)
-},[])
 
 
   
