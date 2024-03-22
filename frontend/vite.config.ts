29ffe5bdf4f08a8ce88eaa0f18658160c50a3a9c
arjun vr <arjarjun2004@gmail.com>
2024-03-23 00:16:28 +0530

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port:5500,
     proxy:{
       '/api':{
-        target:'http://13.233.62.115:8000',
+        target:'http://localhost:8000',
         changeOrigin:true
     }
    }
